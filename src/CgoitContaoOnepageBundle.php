066ab0ed0356764559050b37440bb0c02cc36059
cgoIT <info@cgo-it.de>
2025-02-11 08:41:38 +0100

    chore: fix copyright

--- src/CgoitContaoOnepageBundle.php
+++ src/CgoitContaoOnepageBundle.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of cgoit\contao-onepage-bundle for Contao Open Source CMS.
  *
- * @copyright  Copyright (c) 2024, cgoIT
+ * @copyright  Copyright (c) 2025, cgoIT
  * @author     cgoIT <https://cgo-it.de>
  * @author     Daniel Steuri <https://webrealisierung.ch>
  * @license    LGPL-3.0-or-later
